729c70520671542235ab94dd5730b5c20a3b7778
Matt Douglas <kapoozy@gmail.com>
2015-02-01 10:40:21 -0600

    Added working copy of problem 1G

--- 4_textbook_track/1G_FrequentWordsWithMismatches.py
+++ 4_textbook_track/1G_FrequentWordsWithMismatches.py
@@ -9,22 +9,18 @@ import itertools
 
 # Split the genome into substrings of length, k, starting on intervals starting from 0 to k.
 def count_mer(gen, k):
-
     kmer_list = [''.join(nt) for nt in itertools.product(['A','T','C','G'], repeat=k)]
     kmers = list()
     mismatches = defaultdict(int)
-
+    
     # genome can be split into k reading frames,
     # identify each k-mer in a given frame
-    for i in range(k):  
+    for i in range(k):
         frame = gen[i:]
         kmers += [frame[i:i+k]
-                  for i in range(0, len(frame), k)
+                  for i in range(0, len(frame), k) 
                   if len(frame[i:i+k]) >= k]
     
-    # remove duplicate k-mers
-    #kmers = list(set(kmers))
-
     # count the number of occurances of each k-mer in the genome
     for mer1 in kmer_list:
         for mer2 in kmers:
@@ -33,23 +29,21 @@ def count_mer(gen, k):
                 for nt in range(k):
                     if mer1[nt] != mer2[nt]:
                         diff += 1
-
-                if diff <= d:
-                    mismatches[mer1] += 1
-    
+            if diff <= d:
+                mismatches[mer1] += 1
+                
     return mismatches
-    
+
 # Read file
 with open('rosalind_1g.txt', 'r') as in_file:
-        text = in_file.read().split('\n')
-        genome = text[0]
-        val = text[1].split(' ')
-        k = int(val[0])
-        d = int(val[1])
+    text = in_file.read().split('\n')
+    genome = text[0]
+    val = text[1].split(' ')
+    k = int(val[0])
+    d = int(val[1])
 
 # Find the max number of occurances and the corresponding k-mer(s)
 kmers = count_mer(genome, k)
 max_count = max(kmers.values())
 answer = [key for key, val in kmers.items() if val == max_count]
-print()
 for a in answer: print(a)
