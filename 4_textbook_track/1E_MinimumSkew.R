ccc4f0e59f932716ca780844bc48b686a1c1531f
Matt Douglas <kapoozy@gmail.com>
2015-02-01 19:45:03 -0600

    Updated R script for finding minimum skew
    Split into functions

--- 4_textbook_track/1E_MinimumSkew.R
+++ 4_textbook_track/1E_MinimumSkew.R
@@ -1,35 +1,41 @@
-findSkew <- function(path) {
-      ## ----- Process File ----- ##
-      
-      ## Read in file
-      genome <- readLines(path)
-      
-      ## Get genome length
-      len <- nchar(genome)
-      
-      ## Split genome into characters
-      nucleo = substring(genome, 1:len, 1:len)
+getSkew <- function(fasta) {
+    
+    ## ----- Get header ----- ##
+    header <- substring(fasta[1], 5, 12)
+    
+    ## ----- Process File ----- ##
+    genome <- paste(fasta[2:length(fasta)], collapse="")
+    
+    ## Get genome length
+    len <- nchar(genome)
+    
+    ## Split genome into characters
+    nucleo = substring(fasta, 1:len, 1:len)
+    
+    ## ------ Get Results ------ ##
+    
+    ## Create data frame to hold results
+    results <- vector(length=len)
+    skew <- 0
+    
+    ## Loop through each nucleotide
+    for(i in 1:len) {
+        if(nucleo[i] == "C") {
+              skew <- skew-1
+        }
+        else if(nucleo[i] == "G") {
+              skew <- skew+1
+        }
+        
+        results[i] <- skew
+    }
+    
+    write.table(results, file=paste(header,"_skew.txt", sep=""))
+}
 
-      ## ------ Get Results ------ ##
-      
-      ## Create data frame to hold results
-      results <- vector(length=len)
-      skew <- 0
-      
-      for(i in 1:len) {
-            if(nucleo[i] == "C") {
-                  skew <- skew-1
-            }
-            else if(nucleo[i] == "G") {
-                  skew <- skew+1
-            }
-            
-            results[i] <- skew
-      }
-      
-      answer <- paste("Min:", min(results), "at position", which.min(results), ",", 
-                      "Max:", max(results), "at position", which.max(results))
-      
-      lines(results, type="l", xlab="Position", ylab="Skew")
-      return(answer)
+minSkew <- function(x) {
+    skew <- read.table(x)
+    
+    answer <- paste("Min:", min(skew), "at position", which.min(skew), ",", 
+                    "Max:", max(skew), "at position", which.max(skew))
 }
