8ae1dd48e7db486e5b0f39facf459dfcea5ede12
Matt Douglas <kapoozy@gmail.com>
2015-02-18 21:07:31 -0600

    Update to problem 2D - Attempt #1

--- 4_textbook_track/2D_CountingPeptides.py
+++ 4_textbook_track/2D_CountingPeptides.py
@@ -4,11 +4,49 @@
 # Given: An integer m.
 # Return: The number of linear peptides having integer mass m.
 
-mass = 1024
+'''
+def permu(m, masses):  
+    ways = [0] * (m + 1)
+    ways[0] = 1
+    for mass in mass_list:
+        for j in range(mass, m + 1):
+            ways[j] += ways[j - mass]
+    return ways[m]
 
-def permu(m):
+print(permu(mass, mass_list))
+'''
+try:
+    import psyco
+    psyco.full()
+except ImportError:
+    pass
+ 
+def count_changes(amount_cents, coins):
+    n = len(coins)
+    # max([]) instead of max() for Psyco
+    cycle = max([c+1 for c in coins if c <= amount_cents]) * n
+    table = [0] * cycle
+    for i in range(n+1):
+        table[i] = 1
+ 
+    pos = n
+    for s in range(1, amount_cents + 1):
+        for i in range(n):
+            if i == 0 and pos >= cycle:
+                pos = 0
+            if coins[i] <= s:
+                q = pos - coins[i] * n
+                table[pos]= table[q] if (q >= 0) else table[q + cycle]
+            if i:
+                table[pos] += table[pos - 1]
+            pos += 1
+    return table[pos - 1]
+ 
+def main():
+    mass = 1024
     mass_list = [ 57, 71, 87, 97, 99, 101, 103, 113, 113, 114,
                115, 128, 128, 129, 131, 137, 147, 156, 163, 186 ]
     
-    for i in range(len(mass_list)):
-        sum = 0
+    print(count_changes(mass, mass_list))
+ 
+main()
