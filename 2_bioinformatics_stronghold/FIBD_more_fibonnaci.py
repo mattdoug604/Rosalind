e3a2a70b7866cccd50e3c37b32a8a82f42bf9abc
Matt Douglas <kapoozy@gmail.com>
2015-03-19 19:25:43 -0600

    Delete FIBD_more_fibonnaci.py

--- 2_bioinformatics_stronghold/FIBD_more_fibonnaci.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# Given: Positive integers n ≤ 100 and m ≤ 20.
-# Return: The total number of pairs of rabbits that will remain after the n-th month if all rabbits live for m months.
-
-n = 6 # number of months
-m = 3 # life or a rabbit (in months)
-
