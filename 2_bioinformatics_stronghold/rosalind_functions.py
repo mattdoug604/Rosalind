73f3115cf1d76e8365f5a060d57eabd8123ec2c6
Matt Douglas <kapoozy@gmail.com>
2015-07-19 17:05:08 -0600

    Added check answer function for problem NWCK
    Checks the answer from "rosalind_NWCK" against the answer gotten when
    using the "Tree" module of Biopython (i.e. the correct answer). Used
    when troubleshooting why some answers were +2 in distance greater than
    they should be.

--- 2_bioinformatics_stronghold/rosalind_functions.py
+++ 2_bioinformatics_stronghold/rosalind_functions.py
@@ -20,6 +20,7 @@ def parseFasta(path):
 
     return(fastas)
 
+
 def codonTable(seq_type='dna'):
     ''' Builds a dictionary of codons and corresponding amino acids '''
     bases = ["T", "C", "A", "G"]
@@ -31,3 +32,29 @@ def codonTable(seq_type='dna'):
     codon_table = dict(zip(codons, amino_acids))
 
     return(codon_table)
+
+
+def check_nwck(file):
+    from io import StringIO
+    from Bio import Phylo
+    import networkx
+
+    answers = []
+    
+    with open(file, 'r') as f:
+        lines = map(lambda l: l.strip(), f.readlines())
+        lines = [line for line in lines if line]
+
+    for i in range(int(len(lines)/2)):
+        handle = StringIO(lines[2*i])
+        tree = Phylo.read(handle, "newick")
+        names = lines[2*i+1].split()
+
+        t =  Phylo.to_networkx(tree)
+
+        na = [node for node in t.nodes() if node.name == names[0]][0]
+        nb = [node for node in t.nodes() if node.name == names[1]][0]
+
+        answers.append(len(networkx.shortest_path(t, na, nb))-1)
+
+    return(answers)
