841631a86355f48fa81236b0a74e3439b12b1ef5
Matt Douglas <kapoozy@gmail.com>
2015-07-19 17:05:25 -0600

    Completed problem NWCK

--- 2_bioinformatics_stronghold/rosalind_nwck.py
+++ 2_bioinformatics_stronghold/rosalind_nwck.py
@@ -30,34 +30,82 @@ def find_LCA(t, a, b):
             
             #check if subtree contains both nodes
             if a in sub and b in sub:
-                return(subtrees, level)
+               return(subtrees, level)
+
+    return(subtrees, level)
+
+def distance_to_lca(subtrees, lca, node):
+    binary = False
+    
+    # for each level in the tree...
+    for i in range(lca, len(subtrees)):
+        includes_node = False
+
+        # for each node (and children) in that level...
+        for sub in subtrees[i]:
+
+            if ',' in sub:
+                binary = True
+
+            if node in sub:
+                includes_node = True
+                break
+
+        if includes_node == False:
+            if binary == True:
+                dist = i - lca - 1
+            else:
+                dist = i - lca - 2
+                
+            return(dist)
+
+    if binary == True:
+        dist = len(subtrees) - lca - 1
+    else:
+        dist = len(subtrees) - lca - 2
+        
+    return(dist)
             
 
 def find_distance(t):
     tree = t[0]
-    #a, b = t[1].split(' ')
-    a = 'a'
-    b = 'b'
-    print('find', a, '->', b)
+    a, b = t[1].split(' ')
     
-    subtrees, lvl = find_LCA(tree, a, b)
-    lca = subtrees[lvl]
-    dist = len(subtrees) - (lvl+1)
-    print(subtrees, lca, dist)
+    subtrees, lca = find_LCA(tree, a, b)
+    dist = 2 + distance_to_lca(subtrees, lca, a) + distance_to_lca(subtrees, lca, b)
+    
+    return(dist)
     
 
+def check_answer(ans_a, file):
+    from rosalind_functions import check_nwck
+    
+    ''' Optional: check answers '''
+    mismatches = []
+    
+    ans_b = check_nwck(file)
+    for i in range(len(ans_a)):
+        if ans_a[i] != ans_b[i]:
+            mismatches.append(i)
+
+    if len(mismatches) <= 0:
+        print('no mismatches!\n', ' '.join(map(str, ans_a)), sep='')
+    else:
+        print('mismatches at: ',
+              ' '.join(map(str, mismatches)), '\n',
+              ' '.join(map(str, ans_a)),  '\n',
+              ' '.join(map(str, ans_b)),  sep='')
+
 def main():
-    with open('problem_datasets/rosalind_test2.txt' ,'r') as infile:
+    with open('problem_datasets/rosalind_nwck.txt' ,'r') as infile:
         trees = [l.split('\n') for l in infile.read().strip().split('\n\n')]
 
     answer = []
-    answer.append(find_distance(trees[0]))
-    
-    #for t in trees:
-    #    answer.append(find_distance(t))
+    for t in trees:
+        answer.append(find_distance(t))
 
-    #print(' '.join(map(str, answer)))
-    
+    #check_answer(answer, 'problem_datasets/rosalind_nwck.txt')
+    print(' '.join(map(str, answer)))
 
 if __name__ == '__main__':
     main()
