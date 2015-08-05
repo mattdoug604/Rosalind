2d375a51d7aa5b3dbc4da269c7840d04f6182f7d
Matt Douglas <kapoozy@gmail.com>
2015-08-04 20:51:02 -0600

    Formatting

--- 2_bioinformatics_stronghold/rosalind_PRTM.py
+++ 2_bioinformatics_stronghold/rosalind_PRTM.py
@@ -9,37 +9,27 @@ Given: A protein string P of length at most 1000 aa.
 Return: The total weight of P. Consult the monoisotopic mass table.
 '''
 
-mass_table = { 'A':71.03711,
-               'C':103.00919,
-               'D':115.02694,
-               'E':129.04259,
-               'F':147.06841,
-               'G':57.02146,
-               'H':137.05891,
-               'I':113.08406,
-               'K':128.09496,
-               'L':113.08406,
-               'M':131.04049,
-               'N':114.04293,
-               'P':97.05276,
-               'Q':128.05858,
-               'R':156.10111,
-               'S':87.03203,
-               'T':101.04768,
-               'V':99.06841,
-               'W':186.07931,
-               'Y':163.06333 }
+from rosalind_utils import aa_mass
+
+
+def calc_mass(prot):
+    mass_table = aa_mass()
+    mass = 0
+    
+    for aa in prot:
+        if aa in mass_table.keys():
+            mass += mass_table[aa]
+
+    return(mass)
+
 
 def main():
     with open('problem_datasets/rosalind_prtm.txt', 'r') as infile:
         prot = infile.read().strip()
 
-    mass = float(0)
-    for aa in prot:
-        if aa in mass_table.keys():
-            mass += mass_table[aa]
+    answer = calc_mass(prot)
+    print('%.3f' % answer)
 
-    print('%.3f' % mass)
 
 if __name__ == '__main__':
     main()
