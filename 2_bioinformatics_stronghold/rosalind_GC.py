eefe55ada449e13cdf957d3978f36e8542aabe1a
Matt Douglas <kapoozy@gmail.com>
2015-08-04 19:41:28 -0600

    Answer (a float) is now formatted at the end of the script

--- 2_bioinformatics_stronghold/rosalind_GC.py
+++ 2_bioinformatics_stronghold/rosalind_GC.py
@@ -9,43 +9,27 @@ Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
 Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated; please see the note on absolute error below.
 '''
 
-from __future__ import division
+from rosalind_utils import parse_fasta
 import re
 
-def parseFasta(path):
-    headers = []
-    seqs = {}
-
-    with open(path, 'r') as f:
-        for num, line in enumerate(f):
-            if '>' in line:
-                headers.append(num)
-    headers.append(sum(1 for line in open(path)))
-
-    with open(path, 'r') as f:
-        lines = f.readlines()
-        for i in range(len(headers)-1):
-            h = lines[headers[i]][1:].replace('\n', '')
-            l = lines[headers[i]+1:headers[i+1]]
-            seqs[h] = ''.join(l).replace('\n', '')
-
-    return(seqs)
-
-def computeGC(fastas):
-    max_gc = ['n/a', 0]
+def compute_gc(fastas):
+    max_gc = 0
+    max_h = ''
     
-    for header, seq in fastas.iteritems():
-        gc = '%.6f' % float(((seq.count('G') + seq.count('C')) / len(seq) * 100))
-        if gc > max_gc[1]:
-            max_gc[0] = header
-            max_gc[1] = gc
+    for header, seq in fastas.items():
+        gc = float(((seq.count('G') + seq.count('C')) / len(seq) * 100))
+        if gc > max_gc:
+            max_gc = gc
+            max_h = header
 
-    return(max_gc)
+    return(max_h, max_gc)
+
 
 def main():
-    fastas = parseFasta('problem_datasets/rosalind_gc.txt')
-    max_gc = computeGC(fastas)
-    print('\n'.join(map(str, max_gc)))
+    fastas = parse_fasta('problem_datasets/rosalind_gc.txt')
+    max_h, max_gc = compute_gc(fastas)
+    print(max_h, '\n', '%.6f' % max_gc, sep='')
+
 
 if __name__ == '__main__':
     main()
