00c978d089b2880a4fa84e34d6291ef0aa56abd5
Matt Douglas <kapoozy@gmail.com>
2015-08-04 19:26:45 -0600

    Cleaned up CONS
    renamed functions according to Python Style Guide; condensed some
    functions.

--- 2_bioinformatics_stronghold/rosalind_CONS.py
+++ 2_bioinformatics_stronghold/rosalind_CONS.py
@@ -9,64 +9,42 @@ Given: A collection of at most 10 DNA strings of equal length (at most 1 kbp) in
 Return: A consensus string and profile matrix for the collection. (If several possible consensus strings exist, then you may return any one of them.)
 '''
 
+from rosalind_utils import parse_fasta
 import re
 
-def parseFasta(path):
-    ''' Read a single file containing multiple
-        FASTA sequences, extract only the DNA
-        sequences, and split them into seperate
-        strings.
-    '''
-    seqs = ''
-    with open(path, 'r') as fasta:
-        for line in fasta:
-            if not re.search(r'^>.+$\n', line):
-                seqs += line.strip()
-            else:
-                seqs += '\n'
     
-    seqs = seqs.lstrip().split('\n')
-    return(seqs)
-    
-def profileMatrix(seqs):
-    ''' Generate a profile matrix from a list of
-        DNA sequences. Assumes all the sequences
-        are of equal length.
+def profile_matrix(seqs):
+    ''' Generate a profile matrix from a list of DNA sequences. Assumes all the
+        sequences are of equal length.
     '''
     length = len(seqs[0])
     matrix =[[0 for x in range(4)] for y in range(length)]
-    
+    letters = {'A':0, 'C':1, 'G':2, 'T':3}
+
     for i in range(length):
         for string in seqs:
-            if string[i].upper() == 'A':
-                matrix[i][0] += 1
-            elif string[i].upper() == 'C':
-                matrix[i][1] += 1
-            elif string[i].upper() == 'G':
-                matrix[i][2] += 1
-            elif string[i].upper() == 'T':
-                matrix[i][3] += 1
+            s = string[i]
+            matrix[i][letters[s]] += 1
 
     return(matrix)
 
-def consensusSeq(profile):
-    ''' Determine the consensus sequence from a
-        given profile matrix.
-    '''
-    consensus = ''
 
+def consensus_seq(profile):
+    ''' Determine the consensus sequence from a given profile matrix. '''
+    consensus = ''
     letter = ['A', 'C', 'G', 'T']
+    
     for i in range(len(profile)):
         nt = profile[i].index(max(profile[i]))
         consensus += letter[nt]
 
     return(consensus)
 
-def formatProfile(profile):
-    ''' A generator that outputs a given profile
-        matrix in a readable format.
-    '''
+
+def format_profile(profile):
+    ''' A generator that outputs the profile matrix in a readable format. '''
     prefix = ['A', 'C', 'G', 'T']
+    
     for i in range(4):
         line = prefix[i] + ': '
         for j in range(len(profile)):
@@ -74,15 +52,17 @@ def formatProfile(profile):
 
         yield(line)
 
+
 def main():
-    sequences = parseFasta('problem_datasets/rosalind_cons.txt')
-    profile = profileMatrix(sequences)
-    consensus = consensusSeq(profile)
+    sequences = list(parse_fasta('problem_datasets/rosalind_cons.txt').values())
+    profile = profile_matrix(sequences)
+    consensus = consensus_seq(profile)
 
     with open('output/rosalind_cons_out.txt', 'w') as outfile:
         outfile.write(consensus + '\n')
-        for line in formatProfile(profile):
+        for line in format_profile(profile):
             outfile.write(line + '\n')
+
 
 if __name__ == '__main__':
     main()
