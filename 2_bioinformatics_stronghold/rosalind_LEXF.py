0d116875c9466fd74dac72c8e6701d55792ad61a
Matt Douglas <kapoozy@gmail.com>
2015-08-04 20:00:11 -0600

    Output pointed to wrong folder.

--- 2_bioinformatics_stronghold/rosalind_LEXF.py
+++ 2_bioinformatics_stronghold/rosalind_LEXF.py
@@ -11,20 +11,21 @@ Return: All strings of length n that can be formed from the alphabet, ordered le
 
 from itertools import product
 
+
 def main():
     with open('problem_datasets/rosalind_lexf.txt', 'r') as infile:
         alpha = infile.readline().strip().split(' ')
         n = int(infile.readline())
 
-    ''' ok, I feel like this one is kind of cheating since itertools.product()
-        returns the strings already in the proper order. I'll try and come
-        back and do this 'by hand' at some point (as a learning exercise). '''
+    ''' ok, this one is kind of cheating since itertools.product() returns the
+        strings already in lexographical order.
+    '''
     strings = [''.join(i) for i in product(alpha, repeat=n)]
     
-    with open('rosalind_lexf_out.txt', 'w') as outfile:
+    with open('output/rosalind_lexf_out.txt', 'w') as outfile:
         for s in strings:
-            outfile.write(s)
-            outfile.write('\n')
+            outfile.write(s + '\n')
+
 
 if __name__ == '__main__':
     main()
