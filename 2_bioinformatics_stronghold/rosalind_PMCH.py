62ea83840fc7d1ab7f71990196d03d9dcb83542b
Matt Douglas <kapoozy@gmail.com>
2015-08-04 20:39:22 -0600

    Spacing

--- 2_bioinformatics_stronghold/rosalind_PMCH.py
+++ 2_bioinformatics_stronghold/rosalind_PMCH.py
@@ -11,6 +11,7 @@ Return: The total possible number of perfect matchings of basepair edges in the 
 
 from math import factorial
 
+
 def main():
     with open('problem_datasets/rosalind_pmch.txt', 'r') as infile:
         text = infile.read().strip().split('\n')
@@ -19,6 +20,7 @@ def main():
     perfect = factorial(rna.count('A')) * factorial(rna.count('C'))
     with open('output/rosalind_pmch_out.txt', 'w') as outfile:
         outfile.write(str(perfect))
+
 
 if __name__ == '__main__':
     main()
