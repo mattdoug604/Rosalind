c5ad63e7b6972115e4fb5acd61ee2f58918c4cf0
Matt Douglas <kapoozy@gmail.com>
2015-08-04 19:54:11 -0600

    Formatting

--- 2_bioinformatics_stronghold/rosalind_IPRB.py
+++ 2_bioinformatics_stronghold/rosalind_IPRB.py
@@ -9,21 +9,11 @@ Given: Three positive integers k, m, and n, representing a population containing
 Return: The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele (and thus displaying the dominant phenotype). Assume that any two organisms can mate.
 '''
 
-def main():
-    ''' Calculate the four different cases that
-        would NOT result in an offspring with a
-        dominant allele, then subtract that from
-        1 to get the probability of an offspring
-        that does.
-
-        Dataset contains three integers, in order,
-        corresponding to the number of homozygous
-        dominant, heterozygous, and homozygous
-        recessive individuals.
+def prob(k, m, n):
+    ''' Calculate the four different cases that would NOT result in an offspring with a
+        dominant allele, then subtract that from 1 to get the probability of an
+        offspring that does.
     '''
-    with open('problem_datasets/rosalind_iprb.txt', 'r') as infile:
-        k, m, n = map(float, infile.read().strip().split(' '))
-
     t = k + m + n
     
     c1 = k/t * (k-1)/(t-1) * 1/4
@@ -31,8 +21,22 @@ def main():
     c3 = n/t * k/(t-1) * 1/2
     c4 = n/t * (n-1)/(t-1)
 
-    answer = 1 - (c1 + c2 + c3 + c4)
+    p = 1 - (c1 + c2 + c3 + c4)
+
+    return(p)
+
+
+def main():
+    ''' Dataset contains three integers, in order, corresponding to the number
+        of homozygous dominant, heterozygous, and homozygous recessive
+        individuals.
+    '''
+    with open('problem_datasets/rosalind_iprb.txt', 'r') as infile:
+        k, m, n = map(float, infile.read().strip().split(' '))
+
+    answer = prob(k, m, n)
     print('%.5f' % answer)
+
 
 if __name__ == '__main__': 
     main()
