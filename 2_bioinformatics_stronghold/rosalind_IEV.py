ffddb1103b3f710da0e19aa05b02a8ef75e6c92c
Matt Douglas <kapoozy@gmail.com>
2015-08-04 19:49:12 -0600

    formatting

--- 2_bioinformatics_stronghold/rosalind_IEV.py
+++ 2_bioinformatics_stronghold/rosalind_IEV.py
@@ -17,12 +17,16 @@ Given: Six positive integers, each of which does not exceed 20,000. The integers
 Return: The expected number of offspring displaying the dominant phenotype in the next generation, under the assumption that every couple has exactly two offspring.
 '''
 
+def expected(f):
+    return((f[0]*1 + f[1]*1 + f[2]*1 + f[3]*3/4 + f[4]*1/2 + f[5]*0)*2)
+
+
 def main():
     with open('problem_datasets/rosalind_iev.txt', 'r') as infile:
-        s = [float(i) for i in infile.read().split()]
+        f = [float(i) for i in infile.read().split()]
 
-    expected = (s[0]*1 + s[1]*1 + s[2]*1 + s[3]*3/4 + s[4]*1/2 + s[5]*0)*2
-    print '%.1f' % expected
+    answer = expected(f)
+    print('%.1f' % answer)
     
 if __name__ == '__main__':
     main()
