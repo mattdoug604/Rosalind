2cd33bf1cc9bee97fa32bd6e49a5ae7210398d1a
Matt Douglas <kapoozy@gmail.com>
2015-08-04 19:03:43 -0600

    Added problem ASPC

---
+++ 2_bioinformatics_stronghold/rosalind_ASPC.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+'''
+Rosalind: Bioinformatics Stronghold
+Problem: Introduction to Alternative Splicing
+URL: http://rosalind.info/problems/aspc/
+
+Given: Positive integers n and m with 0 <= m <= n <= 2000.
+Return: The sum of combinations C(n,k) for all k satisfying m <= k <= n, modulo 1,000,000.
+'''
+
+'''
+EXAMPLE INPUT:
+6 3
+
+EXAMPLE OUTPUT:
+42
+'''
+
+from math import factorial
+
+def combinations(n, m):
+    f = factorial
+    return(sum(f(n) // (f(k) * f(n-k)) for k in range(m, n+1)))
+
+    
+def main():
+    with open('problem_datasets/rosalind_aspc.txt', 'r') as infile:
+        n, m = [int(i) for i in infile.readline().strip().split(' ')]
+
+    answer = combinations(n, m) % 1000000
+    print(answer)
+        
+
+if __name__ == '__main__':
+    main()
