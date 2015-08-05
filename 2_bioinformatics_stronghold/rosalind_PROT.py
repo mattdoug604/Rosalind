9c823af56654ad067307ea18259a7fcbd62fcdb9
Matt Douglas <kapoozy@gmail.com>
2015-08-04 20:44:52 -0600

    Formatting

--- 2_bioinformatics_stronghold/rosalind_PROT.py
+++ 2_bioinformatics_stronghold/rosalind_PROT.py
@@ -9,26 +9,33 @@ Given: An RNA string s corresponding to a strand of mRNA (of length at most 10 k
 Return: The protein string encoded by s.
 '''
 
-from rosalind_functions import codonTable
+from rosalind_utils import codon_table
 
-def main():
-    codon_table = codonTable('rna')
 
-    with open('problem_datasets/rosalind_prot.txt', 'r') as infile:
-        rna = infile.read().strip()
-
+def translate(rna):
+    table = codon_table('U')
     peptide = ''
+    
     for nt in range(0, len(rna), 3):
         codon = rna[nt:nt+3]
-        aa = codon_table.get(codon, '*')
+        aa = table.get(codon, '*')
         if aa != '*':
             peptide += aa
         else:
             break
 
+    return(peptide)
+
+        
+def main():
+    with open('problem_datasets/rosalind_prot.txt', 'r') as infile:
+        rna = infile.read().strip()
+
+    answer = translate(rna)
+
     with open('output/rosalind_prot_out.txt', 'w') as outfile:
-        print(peptide)
-        outfile.write(peptide)
+        outfile.write(answer)
+
 
 if __name__ == '__main__':
     main()
