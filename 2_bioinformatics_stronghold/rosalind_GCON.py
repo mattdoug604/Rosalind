15909a7187754ac2dfae40bcd8f700ff8c3534a7
kapoozy <kapoozy@gmail.com>
2015-08-16 15:17:26 -0600

    Completed problem GCON

--- 2_bioinformatics_stronghold/rosalind_GCON.py
+++ 2_bioinformatics_stronghold/rosalind_GCON.py
@@ -39,58 +39,48 @@ def scoring_matrix(path):
     return(costs)
 
 
-def get_score(matrix, a, b, gap=-5):
+def get_score(matrix, a, b):
     ''' Retrieve the score from the scoring matrix. '''
-    if a == '-' or b == '-':
-        return(gap)
-    else:
-        key = ' '.join((a, b))
-        cost = matrix[key]
-
+    key = ' '.join((a, b))
+    cost = matrix[key]
     return(int(cost))
 
 
-def alignment_score(s, t, matrix, gap, gap_e=0):
+def alignment_score(s, t, matrix, gap):
     ''' Returns two matrices of the edit distance and edit alignment between
         strings s and t.
     '''
 
-    # Initialize the matrices with zeros.
-    d = [[0 for j in range(len(t)+1)] for i in range(len(s)+1)]
-    gap_s = [[0 for j in range(len(t)+1)] for i in range(len(s)+1)]
-    gap_t = [[0 for j in range(len(t)+1)] for i in range(len(s)+1)]
+    # Score of best alignment ending with a match or mismatch.
+    M = [[0 for j in range(len(t)+1)] for i in range(len(s)+1)]
+    # Initialize the gap matrices with an arbitrarily small number.
+    # Score of best alignment ending with a space in X.
+    X = [[-9999 for j in range(len(t)+1)] for i in range(len(s)+1)]
+    # Score of best alignment ending with a space in Y.
+    Y = [[-9999 for j in range(len(t)+1)] for i in range(len(s)+1)]
 
-    # Start with gap penalty as False
-    penalty = False
     
     for i in range(1, len(s)+1):
-        d[i][0] = gap
+        M[i][0] = gap
     for j in range(1, len(t)+1):
-        d[0][j] = gap
+        M[0][j] = gap
 
-    # Fill in the score matrix.
-    n = 0
     for i in range(1, len(s)+1):
         for j in range(1, len(t)+1):
-            cost = [ d[i-1][j-1] + get_score(matrix, s[i-1], t[j-1], gap),
-                     d[i-1-n][j] + gap + (n-1)*gap_e,
-                     d[i][j-1-n] + gap + (n-1)*gap_e ]
-            d[i][j] = max(cost)
-            if cost.index(d[i][j]) != 0:
-                n += 1
-            else:
-                n = 0
-
-    print_matrix(d, s, t)
-
+            X[i][j] = max([M[i-1][j] + gap,
+                           X[i-1][j]])
+            Y[i][j] = max([M[i][j-1] + gap,
+                           Y[i][j-1]])
+            M[i][j] = max([M[i-1][j-1] + get_score(matrix, s[i-1], t[j-1]),
+                           X[i][j],
+                           Y[i][j]])
+    
     # The max possible score is found at the bottom-right corner of the matrix
-    return(d[-1][-1])
+    return(M[-1][-1])
 
 
 def main():
-    #s, t = parse_fasta('problem_datasets/rosalind_gcon.txt', True) #2701.891
-    s, t =['PLEASANTLY', 'MEANLY']
-    print('\n'.join((s, t)))
+    s, t = parse_fasta('problem_datasets/rosalind_gcon.txt', True)
     scores = scoring_matrix('data/BLOSUM62.txt')
 
     max_score = alignment_score(s, t, scores, -5)
@@ -99,9 +89,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-'''
-GFFSQNWELFHENEWAYAKYNAEWFSLCKPEHLTKEIGPFVHQECPRPIWVNMMSSILTPDADSYCMIYAHRVQFIAEAVTLTHSYCHENAPHTAMRTHMWGMIIAYDENWDQQTHRQHYTIWDTFHNWHKETPPDGQDNDRTMSCYYQMDPQWCGKFLIREHVDTFHMCNWFFHMFIPNFAYGCRTFNTNNQDASMRHWSKFWVAIIEVSQPRNIQKIFKGATANHDQHEGWAYVSYLRKNWVDGERFIWAFLHKKMLTDSYAKLVMKFLPAGDQWRAQLHEPTEDEYLTQDLITDGGCNESDDTKLYHRFVTDKMRRLICYVGHHALTKTDYECNWYYDTICCAELWQQCVNFIEDNSGYLNDWFANLFFVASEANHFRMQPRHQKCCHGESVTAKCHVQPFSDGDDPRYPCGDQGYLYQQGNYCKYIYTEHSQNHPSQYTPSKKNEFRKYHGDYNEYARHAFKIEIREGNRCWNVWVHVIMVYELLHQCAVALHAIKMQFCRNQDKHWCFRPITTMMTILVIQLFLRHLFSGSAAFHLVWDSCAHCIGPRMEQAAFWAWGTSIHTGCEGAKQSMAGFTARFFDYPVKNHQRKNSVPCRTIQVFPVFKWLWLDRIVDSIQTEWQETHKHENTFHRNPDWVVRIREDSEYNLACKHALPIPALNFCSYWFPAVIIDEHKYPNMKWCQGPEVQPESCEGDKNTKMNFEKMAGWDTSMFAWPMGRPCWPGQMHITEIVRYHSHMEKR
-GFFWQCDSRRLAWENEWAVVMKYNLTQFSLCKPCHYTKEIPFVHQECPMIHNPHVHFDSILTPDADSYCMIYAHRVQVITHSYCHPENAPHKAMRTHMWGMIIAYDEMWDQQTHRQHYTIWDCFINWHKETCYYQMDPQWCGKFLIREHVDTFHMDNWFFFMDTGDYILGIPNFAYGCRFLTNNQDASMRKPPWMDKQCIFSDDRGMMPIIQNHDQWNMMKFLPCEADIKSYLRKNWVDGERFIWALLHQVWNICNCALVMKFNQWQAQLHEPTNCYVRNRAFLITDLQVYVHECYFGQKLRPIHRFVTDNMRRLICYVGHHALTKTDYECNWYRDDLRLPKELDFNMWERFLWWCEMNQQSWESNSGYLNDWFANLFFVASESNHFRMTPRHQYCCHGESVNAKCHVQPFSDGDNPPYWLDGKRKRRYPLYQQYYWMDCKYIYTEHSQNHPLQYTPSSKNEFRKYHGDYNEYARHAFKIQHSACWNYWVRRWSWYKGVILLHQGKYMDHVDCDIFPHRPDKHWVSTYSRCFRPITCMMTILVLFLRGRRILTHETYEWRYAGFSGSEAFVLVWDSCAHCIKPRMEQAAFWAWGTSRCPTHHTHCEGCKQRMAWFHCFTARFFDYISVELGKQKQNSVPCRTIQVIPVFKWLWLDRIVDTHWHQNTFHRNPDWVMKFVMCLACKHALPIVGYDYHHHQALNFCSYWFPAVIIDEEVCPESCEGNKNTKMNFISACEWNKMAGWDTSMLCAWPMGQMHITEIVRYHINHMEKR
-= ~2701
-'''
