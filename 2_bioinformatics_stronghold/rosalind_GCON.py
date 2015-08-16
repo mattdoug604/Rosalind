1b49c470213726b067c72d1ec8a50168d2c55a1e
kapoozy <kapoozy@gmail.com>
2015-08-16 13:55:45 -0600

    Updated GCON

--- 2_bioinformatics_stronghold/rosalind_GCON.py
+++ 2_bioinformatics_stronghold/rosalind_GCON.py
@@ -22,7 +22,33 @@ EXAMPLE OUTPUT:
 13
 '''
 
-from rosalind_utils import parse_fasta, scoring_matrix, align_score, print_matrix
+from rosalind_utils import parse_fasta, print_matrix
+
+def scoring_matrix(path):
+    ''' Format a mutli-line text file containing substitution scores
+        into a dictonary.
+    '''
+    with open(path, 'r') as f:
+        lines = f.read().strip().split('\n')
+
+    costs = {}
+    for i in lines:
+        l = i.split(' ')
+        costs[' '.join(l[:2])] = l[2]
+
+    return(costs)
+
+
+def get_score(matrix, a, b, gap=-5):
+    ''' Retrieve the score from the scoring matrix. '''
+    if a == '-' or b == '-':
+        return(gap)
+    else:
+        key = ' '.join((a, b))
+        cost = matrix[key]
+
+    return(int(cost))
+
 
 def alignment_score(s, t, matrix, gap, gap_e=0):
     ''' Returns two matrices of the edit distance and edit alignment between
@@ -46,7 +72,7 @@ def alignment_score(s, t, matrix, gap, gap_e=0):
     n = 0
     for i in range(1, len(s)+1):
         for j in range(1, len(t)+1):
-            cost = [ d[i-1][j-1] + align_score(matrix, s[i-1], t[j-1], gap),
+            cost = [ d[i-1][j-1] + get_score(matrix, s[i-1], t[j-1], gap),
                      d[i-1-n][j] + gap + (n-1)*gap_e,
                      d[i][j-1-n] + gap + (n-1)*gap_e ]
             d[i][j] = max(cost)
@@ -55,14 +81,14 @@ def alignment_score(s, t, matrix, gap, gap_e=0):
             else:
                 n = 0
 
-    #print_matrix(d, s, t)
+    print_matrix(d, s, t)
 
     # The max possible score is found at the bottom-right corner of the matrix
     return(d[-1][-1])
 
 
 def main():
-    #s, t = parse_fasta('problem_datasets/rosalind_gcon.txt', seqs_only=True) #2701.891
+    #s, t = parse_fasta('problem_datasets/rosalind_gcon.txt', True) #2701.891
     s, t =['PLEASANTLY', 'MEANLY']
     print('\n'.join((s, t)))
     scores = scoring_matrix('data/BLOSUM62.txt')
