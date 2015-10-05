5c6c8363a92d9a02a1a63f6389d65237a676443b
kapoozy <kapoozy@gmail.com>
2015-10-05 17:25:59 -0600

    Added problem MOTZ

--- 2_bioinformatics_stronghold/rosalind_CAT.py
+++ 2_bioinformatics_stronghold/rosalind_CAT.py
@@ -36,22 +36,22 @@ def count_matchings(i, j):
     elif j == 1 and match[s[i] == s[j]]:
         result = 1
     else:
-        result = sum([(count_matchings(i+1, k-1) * count_matchings(k+1, j))
-                      for k in range(i+1, j+1, 2)
-                      if match[s[i]] == s[k]])
-
+        for k in range(i+1, j+1, 2):
+            if s[k] == match[s[i]]:
+                result += count_matchings(i+1, k-1) * count_matchings(k+1, j)
+        
     pairs[i][j] = result
     
     return result
 
 
 if __name__ == '__main__':
-    with open('problem_datasets/rosalind_cat.txt', 'r') as infile:
-        next(infile)
-        s = infile.read().replace('\n', '')
-
     match = {'A':'U','U':'A', 'C':'G', 'G':'C'}
     pairs = [[-1 for x in range(len(s)+1)] for y in range(len(s)+1)]
     
+    with open('problem_datasets/rosalind_cat.txt', 'r') as infile:
+        next(infile)
+        s = infile.read().replace('\n', '')
+        
     print(count_matchings(0, len(s)-1) % 1000000)
     
