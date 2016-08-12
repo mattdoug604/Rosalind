ca5e536594e21f6bbba8c5e8ce70a41716d388f9
kapoozy <mattdoug604@gmail.com>
2016-08-12 15:33:10 -0700

    Added more perl implementations of my answers, and a few tweaks to existing python ones.

--- 2_bioinformatics_stronghold/rosalind_ASMQ.py
+++ 2_bioinformatics_stronghold/rosalind_ASMQ.py
@@ -5,37 +5,38 @@ Rosalind: Bioinformatics Stronghold
 Problem: Assessing Assembly Quality with N50 and N75
 URL: http://rosalind.info/problems/asmq/
 
-Given: A collection of at most 1000 DNA strings (whose combined length does 
+Given: A collection of at most 1000 DNA strings (whose combined length does
 not exceed 50 kbp).
 
 Return: N50 and N75 for this collection of strings.
 '''
 
-def Nxx(dna, xx):
-    # Create a list containing n copies of an integer, n; where n is the 
-    # length of each given string in a list. 
-    lenlist = []
-    for i in dna:
-        lenlist += [len(i)]*len(i)
-    lenlist = sorted(lenlist)
-        
+def Nxx(lenlist, xx):
     # Take the mean of the two middle elements if there are an even number
     # of elements. Otherwise, take the middle element.
     n = 100 / (100 - xx)
+    medianpos = int(len(lenlist) / n)
+    print medianpos
     if len(lenlist) % 2 == 0:
-        medianpos = int(len(lenlist) / n)
         return lenlist[medianpos] + lenlist[medianpos-1] / n
     else:
-        medianpos = int(len(lenlist) / n)
         return lenlist[medianpos]
 
 
 def main():
     with open('problem_datasets/rosalind_asmq.txt', 'r') as infile:
         dna = infile.read().strip().split('\n')
-    
-    print(Nxx(dna, 50), Nxx(dna, 75))
-    
-        
+
+    # Create a list containing n copies of an integer, n, where n is the
+    # length of each given string in a list.
+    lenlist = []
+    for i in dna:
+        lenlist += [len(i)]*len(i)
+    lenlist = sorted(lenlist)
+    print len(lenlist)
+
+    print(Nxx(lenlist, 50), Nxx(lenlist, 75))
+
+
 if __name__ == '__main__':
     main()
--- 2_bioinformatics_stronghold/rosalind_CAT.py
+++ 2_bioinformatics_stronghold/rosalind_CAT.py
@@ -2,7 +2,7 @@
 
 '''
 Rosalind: Bioinformatics Stronghold
-Problem: Catalan Numbers and RNA Secondary Structures 
+Problem: Catalan Numbers and RNA Secondary Structures
 URL: http://rosalind.info/problems/cat/
 
 Given: An RNA string s having the same number of occurrences of 'A' as 'U' and
@@ -24,28 +24,28 @@ EXAMPLE OUTPUT:
 def count_matchings(i, j):
     if pairs[i][j] != -1:
         return(pairs[i][j])
-    
+
     result = 0
     if i > j:
         result = 1
     elif j == 1 and match[s[i] == s[j]]:
-        result = 1
+		result = 1
     else:
         for k in range(i+1, j+1, 2):
             if s[k] == match[s[i]]:
                 result += count_matchings(i+1, k-1) * count_matchings(k+1, j)
-        
+
     pairs[i][j] = result
     return result
 
 
 if __name__ == '__main__':
     match = {'A':'U','U':'A', 'C':'G', 'G':'C'}
-    
+
     with open('problem_datasets/rosalind_cat.txt', 'r') as infile:
         infile.readline()
         s = infile.read().replace('\n', '')
-    
+
     pairs = [[-1 for x in range(len(s)+1)] for y in range(len(s)+1)]
-       
+
     print(count_matchings(0, len(s)-1) % 1000000)
