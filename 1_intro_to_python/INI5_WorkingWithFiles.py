3078f5a1c3a98a6a709bffef2fe5b034fc7f5588
mattdoug604 <mattdoug604@gmail.com>
2021-04-03 20:09:43 -0700

    standardize problem dataset file paths

--- 1_intro_to_python/INI5_WorkingWithFiles.py
+++ 1_intro_to_python/INI5_WorkingWithFiles.py
@@ -2,7 +2,7 @@
 # Given: A file containing at most 1000 lines.
 # Return: A file containing all the even-numbered lines from the original file. Assume 1-based numbering of lines.
 
-filename="rosalind_ini5.txt"
+filename="problem_datasets/rosalind_ini5.txt"
 counter = 0
 
 for line in open(filename, "r"):
