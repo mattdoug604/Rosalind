91f20d78dd8f29c578bea97dd6eef13e8c1dc4f4
mattdoug604 <mattdoug604@gmail.com>
2021-04-03 21:57:15 -0700

    INI6 - do not use reserved word 'dict' as var name

--- 1_intro_to_python/INI6_Dictionaries.py
+++ 1_intro_to_python/INI6_Dictionaries.py
@@ -4,15 +4,15 @@ Given:  A string s of length at most 10000 letters.
 Return: How many times any word occurred in string. Each letter case (upper or lower) in word matters. Lines in output can be in any order.
 """
 s = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom let it be And in my hour of darkness she is standing right in front of me Speaking words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the broken hearted people living in the world agree There will be an answer let it be For though they may be parted there is still a chance that they will see There will be an answer let it be Let it be let it be let it be let it be There will be an answer let it be Let it be let it be let it be let it be Whisper words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the night is cloudy there is still a light that shines on me Shine until tomorrow let it be I wake up to the sound of music Mother Mary comes to me Speaking words of wisdom let it be Let it be let it be let it be yeah let it be There will be an answer let it be Let it be let it be let it be yeah let it be Whisper words of wisdom let it be"
-dict = {}
+result = {}
 
 for word in s.split(" "):
-    if word in dict.keys():
-        dict[word] += 1
+    if word in result.keys():
+        result[word] += 1
     else:
-        dict[word] = 1
+        result[word] = 1
 
-for key, value in dict.items():
+for key, value in result.items():
     print(key, value)
 
 # Note: this doesn't acount for upper/lower case letters, eg) 'We' and 'we' are seen as different words
